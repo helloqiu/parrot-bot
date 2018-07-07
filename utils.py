fbcb9fe8eff558341a2107d593124d558b64cc5c
helloqiu <helloqiu95@gmail.com>
2018-07-07 17:57:36 +0800

    fix(utils): fix response

--- utils.py
+++ utils.py
@@ -3,7 +3,7 @@
 
 def check_admin(group_id, user_id, bot):
     result = bot.get_group_member_info(group_id=group_id, user_id=user_id)
-    if result.role in ['owner', 'admin']:
+    if result['role'] in ['owner', 'admin']:
         return True
     else:
         return False
