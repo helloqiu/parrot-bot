c66caaba30fe4dde280cd15e50204c2dd72a43c3
Yuyang Zhang <helloqiu95@gmail.com>
2018-07-07 16:36:46 +0800

    feat(bot): init implementation

---
+++ utils.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+
+
+def check_admin(group_id, user_id, bot):
+    result = bot.get_group_member_info(group_id, user_id)
+    if result.role in ['owner', 'admin']:
+        return True
+    else:
+        return False
