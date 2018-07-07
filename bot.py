9a7330f210fbc1fa4d138910adc049f4929d57e8
helloqiu <helloqiu95@gmail.com>
2018-07-07 18:22:49 +0800

    feat(change message):

--- bot.py
+++ bot.py
@@ -6,8 +6,6 @@ from gevent.pywsgi import WSGIServer
 import random
 import logging
 
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
 bot = CQHttp(api_root='http://127.0.0.1:5700')
 bot_config = {
     'rate': 0.1
@@ -27,15 +25,15 @@ def handle_group_message(context):
     # check if the user is admin
     if_admin = check_admin(context['group_id'], context['user_id'], bot)
     if not if_admin and not context['anonymous']:
-        logging.info(context['raw_message'])
-        logging.info(last_message.message)
+        logging.debug(context['raw_message'])
+        logging.debug(last_message.message)
         if last_message.check(context['raw_message']) and random.choice([True, False]):
             # 禁言！
-            logging.info('禁言！')
+            logging.debug('禁言！')
             bot.set_group_ban(group_id=context['group_id'], user_id=context['user_id'], duration=60 * 5)
             bot.send_group_msg(
                 group_id=context['group_id'],
-                message='[CQ:at,qq={}]这是一只鹦鹉[CQ:emoji,id=128536]'.format(context['user_id'])
+                message='[CQ:at,qq={}]\n这是一只鹦鹉[CQ:emoji,id=128536]'.format(context['user_id'])
             )
         else:
             # 变成复读机！
