79e988886969f2347f5c37a0029a62498beabe6a
Yuyang Zhang(helloqiu) <helloqiu95@gmail.com>
2018-07-17 22:19:35 +0800

    feat(session): add sqlite session (#3)
    * feat(session): add sqlite session
    
    * feat(session): use session to store message
    
    * fix(bot): fix last_message
    
    * feat(bot): change repeat rate to 0.05

--- bot.py
+++ bot.py
@@ -1,23 +1,25 @@
 # -*- coding: utf-8 -*-
 
 from cqhttp import CQHttp
-from utils import check_admin, Message
+from utils import check_admin, SQLiteStorage
 from gevent.pywsgi import WSGIServer
 import random
 import logging
 
 bot = CQHttp(api_root='http://127.0.0.1:5700')
 bot_config = {
-    'rate': 0.3
+    'rate': 0.05,
+    'ban_duration': 60 * 5
 }
 
-last_message = Message()
+session = SQLiteStorage()
+session.config = bot_config
 
 
 @bot.on_event('group_increase')
 def handle_group_increase(context):
     logging.info('有新人!')
-    bot.send(context, message='欢迎新人！\n请先关注群公告哦~', is_raw=True)
+    bot.send(context, message='欢迎新人！\n请先关注群公告哦~')
 
 
 @bot.on_message('group')
@@ -25,13 +27,18 @@ def handle_group_message(context):
     # check if the user is admin
     if_admin = check_admin(context['group_id'], context['user_id'], bot)
     if not context['anonymous']:
+        last_message = session.get(context['group_id'])
         logging.debug(context['raw_message'])
         logging.debug(last_message.message)
-        if last_message.check(context['raw_message']) and random.choice([True, False]):
+        if last_message and last_message['message'] == context['raw_message'] and random.choice([True, False]):
             if not if_admin:
                 # 禁言！
                 logging.debug('禁言！')
-                bot.set_group_ban(group_id=context['group_id'], user_id=context['user_id'], duration=60 * 5)
+                bot.set_group_ban(
+                    group_id=context['group_id'],
+                    user_id=context['user_id'],
+                    duration=session.config['ban_duration']
+                )
                 bot.send_group_msg(
                     group_id=context['group_id'],
                     message='[CQ:at,qq={}]\n这是一只鹦鹉[CQ:emoji,id=128536]'.format(context['user_id'])
@@ -45,11 +52,12 @@ def handle_group_message(context):
                 )
         else:
             # 变成复读机！
-            if random.randint(0, 9) < bot_config['rate'] * 10:
+            if random.randint(0, 9) < session.config['rate'] * 10:
                 bot.send_group_msg(
                     group_id=context['group_id'],
                     message=context['message']
                 )
+        session.set(context['group_id'], {'message': context['raw_message']})
 
 
 http_server = WSGIServer(('127.0.0.1', 8080), bot._server_app)
